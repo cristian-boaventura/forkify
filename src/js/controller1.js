4dae50108a479ad423ac02f71ca98a209145596d
cristian-boaventura <96512124+cristian-boaventura@users.noreply.github.com>
2022-06-18 21:31:19 -0300

    Added new welcome feature

--- src/js/controller1.js
+++ src/js/controller1.js
@@ -128,6 +128,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('Welcome to the application');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerrender(controlRecipes);
@@ -136,6 +140,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('Welcome');
+  newFeature();
 };
 init();
