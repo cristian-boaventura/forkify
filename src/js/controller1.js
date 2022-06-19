29f5e3d6291310716d89ee2c988e2f62e84ae3d5
cristian-boaventura <96512124+cristian-boaventura@users.noreply.github.com>
2022-06-18 21:39:27 -0300

    new feature deleted

--- src/js/controller1.js
+++ src/js/controller1.js
@@ -128,10 +128,6 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
-const newFeature = function () {
-  console.log('Welcome to the application');
-};
-
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerrender(controlRecipes);
@@ -140,6 +136,5 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  newFeature();
 };
 init();
