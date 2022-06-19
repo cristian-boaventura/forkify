faf85b40164dc5996dd70a93cf4b2d681dd7fce9
cristian-boaventura <96512124+cristian-boaventura@users.noreply.github.com>
2022-06-18 22:37:22 -0300

    test deleted

--- src/js/controller1.js
+++ src/js/controller1.js
@@ -136,6 +136,5 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('Netlify test');
 };
 init();
