13b1bdae27b03834edc2e7b188627b3383ea4fcc
cristian-boaventura <96512124+cristian-boaventura@users.noreply.github.com>
2022-09-15 04:35:00 -0300

    reset app

--- src/js/config.js
+++ src/js/config.js
@@ -4,6 +4,6 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '83d2ae40-c99c-4b11-8b68-d4737d2b53aa';
+export const KEY = 'b754bc93-bc46-4e32-bb56-b12c1956aa59';
 export const MODAL_CLOSE_SEC = 2.5;
 export const ADD_RECIPE_RERENDER_SEC = 3;
